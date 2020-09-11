3e46a406dec5e0880fad40c3965c6dd3e82bb975
Ricardo <ricardo.dalcinh@gmail.com>
2020-09-11 18:08:59 -0300

    Setup unform

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import {
   Image,
   View,
@@ -8,6 +8,8 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -24,7 +26,12 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
+
+  const handleSignIn = useCallback((data: any) => {
+    console.log(data);
+  }, []);
 
   return (
     <>
@@ -44,12 +51,14 @@ const SignIn: React.FC = () => {
               <Title>Faça seu logon</Title>
             </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+            </Form>
 
             <Button
               onPress={() => {
-                console.log('press');
+                formRef.current?.submitForm();
               }}
             >
               Entrar
@@ -66,7 +75,7 @@ const SignIn: React.FC = () => {
 
           <CreateAccountButton
             onPress={() => {
-              navigation.goBack();
+              navigation.navigate('SignUp');
             }}
           >
             <Icon name="log-in" size={20} color="#ff9000" />
